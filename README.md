fb113868a4571efe6784aef5340692974005e89e
Andrew <andxyz.stevens@gmail.com>
2012-08-03 19:13:04 -0400

    friday before the long weekend and things spin up
    I'm happy
     - Lists some cards out on the screen, making my own meteor collection
     - essentially mirrors the todoItems but without the doneCheckbox nor the private magic

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Mello
 
-A task manager app, inspired by Trello, running on Meteor JS
+A task manager app, inspired by Trello, running on Meteor JS (auth branch)
+
+## Todo (kiss - baby steps stupid)
+ - List some cards out on the screen, making my own meteor collection
+ - essentially mirrors the todoItems but without the doneCheckbox nor the private magic
+
 
---
+++ server/publish.js
@@ -0,0 +1,36 @@
+// Lists -- {name: String}
+Lists = new Meteor.Collection("lists");
+
+// Publish complete set of lists to all clients.
+Meteor.publish('lists', function () {
+  return Lists.find();
+});
+
+// Todos -- {text: String,
+//           done: Boolean,
+//           tags: [String, ...],
+//           list_id: String,
+//           timestamp: Number}
+Todos = new Meteor.Collection("todos");
+
+// Publish visible items for requested list_id.
+Meteor.publish('todos', function (list_id) {
+  return Todos.find({
+    list_id: list_id,
+    privateTo: {
+      $in: [null, this.userId()]
+    }
+  });
+});
+
+// Cards -- { text: String,
+//			  list_id: String
+//            timestamp: Number }
+Cards = new Meteor.Collection("cards");
+
+// Publish visible items for requested list_id.
+Meteor.publish('cards', function (list_id) {
+  return Cards.find({
+    list_id: list_id
+  });
+});
