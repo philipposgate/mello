3efdb16a6784f27a60a4e34bc89bc75716f96084
philipposgate <philip.posgate@gmail.com>
2012-08-25 13:00:23 -0400

    better login detection

--- client/client.js
+++ client/client.js
@@ -1,26 +1,17 @@
-Session.set('loggedIn', null);
-
 Meteor.startup(function(){
 	console.log("startup: " + new Date());
 	Backbone.history.start({pushState: true});
-	Session.set('loggedIn', !_.isEmpty(Meteor.user()));
 });
 
 function onLogin()
 {
 	console.log("onLogin(): " + new Date());
-	Session.set('loggedIn', true);
 }
 
 function onLogout()
 {
 	console.log("onLogout(): " + new Date());
-	Session.set('loggedIn', false);
 }
-
-Template.main.loggedIn = function() {
-	return Session.get('loggedIn');
-};
 
 Template.home.greeting = function () {
   return "Watch the client & server consoles while you click this button: ";
