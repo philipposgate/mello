ccbb57cab66b780f6cab32fef32a0e90897548f0
Slingr <liquidd2@yahoo.co.uk>
2012-09-18 00:30:18 -0400

    Add prettyPrint helper to db admin so documents retrieved from db and printed nicely

--- violet/client/violet.client.js
+++ violet/client/violet.client.js
@@ -7,6 +7,23 @@ Template.violet.events = {
 
 Template.violet.created = getCollectionNames;
 
+Template.documents.prettyPrint = function(object, key) {
+	var o = $('<div class="vio-object">');
+	$(o).append($('<div>' + (key != "null" ? key+": ":"") + '{</div>'));
+	
+	_.each(object, function(value, key) {
+		if(_.isObject(value)) {
+			$(o).append((Template.documents.prettyPrint(value, key)).string);
+		} else {
+			var keyVal = $('<div class="vio-kv">').html(key + ' : "' + object[key] + '"');
+			$(o).append(keyVal);
+		}
+	})
+	
+	$(o).append($('<div>}</div>'));
+	return new Handlebars.SafeString("<div class='vio-obj'>" + $(o).html() + "</div>");
+}
+
 function getCollectionNames() {
 	Meteor.call('getCollectionNames', function(error, result) {
 		if (typeof error === "undefined") {
@@ -21,7 +38,9 @@ function getCollectionInfo(collection) {
 		if (typeof error === "undefined") {			
 			Template.documents.list = [];
 			_.each(result, function(value) {
-				Template.documents.list.push(JSON.stringify(value));
+				value.address= {};
+				value.address.street = "123 Street";
+				Template.documents.list.push(value);
 			})
 			
 			$('.documentList').html(Template.documents());
